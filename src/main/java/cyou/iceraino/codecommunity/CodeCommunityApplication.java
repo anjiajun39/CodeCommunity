e86157daf1ca7003e36d188df003f69d5561591c
anjiajun39 <iceraino@icloud.com>
2022-03-01 21:47:16 +0800

    init repo

---
+++ src/main/java/cyou/iceraino/codecommunity/CodeCommunityApplication.java
@@ -0,0 +1,13 @@
+package cyou.iceraino.codecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cyou/iceraino/codecommunity/CodeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cyou.iceraino.codecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
