ab570175bd5b073dc6b07a94134245c91120ec10
anjiajun39 <iceraino@icloud.com>
2022-03-04 12:35:42 +0800

    Add cookies and session.

--- src/main/java/cyou/iceraino/codecommunity/Controller/AuthorizeController.java
+++ src/main/java/cyou/iceraino/codecommunity/Controller/AuthorizeController.java
@@ -2,27 +2,58 @@ package cyou.iceraino.codecommunity.Controller;
 
 import cyou.iceraino.codecommunity.dto.AccessTokenDTO;
 import cyou.iceraino.codecommunity.dto.GithubUser;
+import cyou.iceraino.codecommunity.mapper.UserMapper;
+import cyou.iceraino.codecommunity.model.User;
 import cyou.iceraino.codecommunity.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code){
+    public String callback(@RequestParam(name = "code") String code,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("a17da84f153c873b2563");
-        accessTokenDTO.setClient_secret("e5ac2fbf0fd0d77695eae349305c728c124f94ea");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功,写入cookie和session
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 }
